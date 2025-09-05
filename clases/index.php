e529312af754bc6a8a6e2ed317ce470da786f2b6
miguel <miguelcruz201715@gmail.com>
2025-09-05 18:38:32 -0500

    x

---
+++ clases/index.php
@@ -0,0 +1,56 @@
+<?php
+//clase: molde para crear mas objetos
+
+class carro{
+    //atributos o propiedades (variables)
+    public $color= 'rojo';
+    public $marca= 'ferrari';
+    public $modelo= 'aventador';
+    public $velocidad= 500;
+    public $caballaje= 300;
+    public $puestos=2;
+
+    //metodo -> acciones que hace al carro(funciones)
+
+    public function getcolor(){
+        return $this->color;
+    }
+
+    public function setColor($color){
+    $this->color=$color;
+
+}
+
+public function getvelocidad(){
+    return $this->velocidad;
+}
+
+
+
+public function acelerar(){
+    $this->velocidad++;
+}
+
+public function frenar(){
+    $this->velocidad--;
+}
+}
+
+//crear un objeto  
+$carroEspecial= new carro();
+
+//var_dump($carroEspecial);
+echo 'la velocidad de mi carro es;:' .$carroEspecial->getVelocidad().'<br>';
+
+echo 'el color de mi carro es:'.$carroEspecial->getColor().'<br>';
+
+$carroEspecial->setColor('amarillo');
+
+echo $carroEspecial->getColor().'<br>';
+
+$carroEspecial->acelerar();
+$carroEspecial->acelerar();
+$carroEspecial->acelerar();
+$carroEspecial->acelerar();
+
+echo 'ahora la velocidad de mi carro es'. $carroEspecial->getVelocidad().'<br>';
